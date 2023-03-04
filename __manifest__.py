7d656affa94d259d463fef50637aed4a0f0a2ace
unknown <oumardiallo22330000@gmail.com>
2023-03-04 15:46:08 +0100

    commit de report

--- __manifest__.py
+++ __manifest__.py
@@ -23,8 +23,14 @@ employés, des vols et des appareils, et d'autre part des clients et des billets
         'views/vol_views.xml',
         'views/ligne_views.xml',
         'views/avion_views.xml',
-        'views/aeroport_views.xml'
-    ],
+        'views/aeroport_views.xml',
+        'report/vol_report.xml',
+        'report/aeroport_report_temlpate.xml',
+        'report/aeroport_report.xml',
+        'report/pilote_template.xml',
+        'report/pilote_report.xml',
+        'report/equipage_report.xml',   
+         ],
     'demo': [
     ],
     'application': True,
