a63cef3fb35204e59e41eebb76c6042df7ede27b
diallomar <oumardiallo22330000@gmail.com>
2023-03-06 12:45:54 +0000

    commit

--- models/depart.py
+++ models/depart.py
@@ -13,10 +13,6 @@ class Depart(models.Model):
     reservation_ids = fields.One2many(
         'aeroport.reservation', 'depart_id', 'Décolage')
 
-    # message_template_id = fields.Many2one(
-    #     'mail.template', string='Message Template')
-
-    # validity = fields.Boolean(string='validite')
 
     def name_get(self):
         res = []
@@ -24,11 +20,6 @@ class Depart(models.Model):
             name = 'Départ du Vol N° ' + depart.vol_id.number
             res.append((depart.id, name))
         return res
-
-    # @api.onchange('vol_id')
-    # def get_validity(self):
-    #     val = self.env['aeroport.vol'].search([])
-    #     self.validity = val.validity
 
     @api.model
     def create(self, vals):
@@ -40,10 +31,4 @@ class Depart(models.Model):
                 template.id).sudo().send_mail(depart.id, force_send=True)
             self.env['mail.mail'].sudo().process_email_queue()
 
-            # template.send_mail(depart.id, force_send=True, email_values={
-            #     'email_from': 'consultationpatient@gmail.com',
-            #     'email_to': depart.pilote_id.email,
-            #     'subject': 'Nouveau départ',
-            #     'body_html': 'Bonjour {},<br/>Un nouveau départ a été créé pour le vol {} du {} <br/>.'.format(depart.pilote_id.name, depart.vol_id.number, depart.date),
-            # })
         return depart
